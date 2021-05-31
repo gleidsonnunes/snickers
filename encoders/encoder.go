9862afa41fe953a9b9a2bfd8e2a211c3cb166ae6
Gleidson Rodrigues Nunes <gleidson-nunes@hotmail.com>
2021-05-31 19:17:26 -0300

    Remove HLS

--- encoders/encoder.go
+++ encoders/encoder.go
@@ -13,8 +13,5 @@ type EncodeFunc func(logger lager.Logger, dbInstance db.Storage, jobID string) e
 // GetEncodeFunc returns the encode function
 // based on the job.
 func GetEncodeFunc(job types.Job) EncodeFunc {
-	if job.Preset.Container == "m3u8" {
-		return HLSEncode
-	}
 	return FFMPEGEncode
 }
--- encoders/encoders_test.go
+++ encoders/encoders_test.go
@@ -11,19 +11,6 @@ import (
 
 var _ = Describe("Encoders", func() {
 	Context("GetEncodeFunc", func() {
-		It("should return HLSEncode if container is m3u8", func() {
-			job := types.Job{
-				ID:          "123",
-				Source:      "ftp://login:password@host/source_here.mov",
-				Destination: "s3://user@pass:/bucket/",
-				Preset:      types.Preset{Name: "240p", Container: "m3u8"},
-				Status:      types.JobCreated,
-			}
-			encodeFunc := GetEncodeFunc(job)
-			funcName := runtime.FuncForPC(reflect.ValueOf(encodeFunc).Pointer()).Name()
-			Expect(funcName).To(Equal("github.com/gleidsonnunes/snickers2/encoders.HLSEncode"))
-		})
-
 		It("should return FFMPEGEncode if source is not m3u8", func() {
 			job := types.Job{
 				ID:          "123",
